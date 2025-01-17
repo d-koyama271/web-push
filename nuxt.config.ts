05efa4e0adb52a4b6c73646236adcb17f78dd062
d-koyama <d-koyama@capsule-co.jp>
2025-01-17 18:16:10 +0900

    add gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
   app: {
     baseURL: "/web-push/",
   },
+  app:{
+    head: {
+      htmlAttrs:{
+        lang: 'ja'
+      },
+      title: "通知デモ",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
 
   modules: ["@vite-pwa/nuxt"],
 
