16ae1b1b0562a3e324b05fd8b1412b499ad69475
d-koyama <d-koyama@capsule-co.jp>
2025-05-16 18:39:37 +0900

    fix

--- api/heartbeat.js
+++ api/heartbeat.js
@@ -2,7 +2,12 @@ import { Redis } from "@upstash/redis";
 
 const redis = Redis.fromEnv();
 
-export default async () => {
-  await redis.ping();
-  return new Response("pong", { status: 200 });
+export default async (req, res) => {
+  try {
+    await redis.ping({ timeout: 3000 });
+    res.status(200).end('pong');
+  } catch (e) {
+    console.error(e);
+    res.status(500).end('err');
+  }
 };
